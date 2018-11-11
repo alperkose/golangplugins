00d9af6d167e889ad039764ae4dc24493a409ce2
Alper Köse <alperkose@gmail.com>
2018-11-11 17:25:33 +0100

    re-arrange the output to reflect the same ordering with the article in medium

--- run.sh
+++ run.sh
@@ -3,17 +3,21 @@
 GO_VERSION=${1:-1.11}
 docker build -q -t golangplugins-example --build-arg GO_VERSION=$GO_VERSION .
 
-echo Go version
+echo \# Go version
+echo ------------
 docker run --entrypoint go golangplugins-example version
 echo 
 
-echo Running with non-vendored implementation
+echo \# Running with non-vendored implementation
+echo ------------------------------------------
 docker run golangplugins-example -plugin simpleuser.plugin
 echo 
 
-echo Running with vendored implementation
+echo \# Running with implementation in different gopath
+echo -------------------------------------------------
+docker run golangplugins-example -plugin differentgopath.plugin
+
+echo \# Running with vendored implementation
+echo --------------------------------------
 docker run golangplugins-example -plugin vendoreduser.plugin
 echo 
-
-echo Running with implementation in different gopath
-docker run golangplugins-example -plugin differentgopath.plugin
